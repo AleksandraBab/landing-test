14a6d1e79db16310f6e82e25e05c299e4eba3b41
AleksandraBab <alex-elm@yandex.ru>
2021-03-17 02:02:00 +0300

    добавлен js-файл

---
+++ index.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper-container', {
+  speed: 2500,
+  spaceBetween: 30,
+  effect: 'fade',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
